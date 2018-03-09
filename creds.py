96f186b1d1726797b30a24670598b09b7db92d1a
mlsamuelson <mlsamuel@asu.edu>
2018-03-09 14:05:12 -0700

    Parameterize post-auth redirect and welcome page.

--- creds.py
+++ creds.py
@@ -7,3 +7,13 @@ Product_ID = ""
 
 #Redis
 redis_url = ""
+
+#Misc Configs
+post_auth_redirect = "https://dev-ask-asu.ws.asu.edu"  # "/"
+welcome_page = "/static/help.html"  # "/static/welcome.html"
+# Ring colors
+# color_standby = ""
+# color_listening = ""
+# color_thinking = ""
+# color_responding = ""
+# color_error = ""
